5a8670dc68b9f661f9698e292112434c106aa5e6
atemcozz <atemcozzartem@gmail.com>
2022-08-05 12:19:38 +0300

    .

--- src/components/ActionSideMenu/ActionSideMenu.jsx
+++ src/components/ActionSideMenu/ActionSideMenu.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import NavActions from "../UI/Sidemenu/NavActions";
 const ActionSideMenu = () => {
   return (
-    <aside className="hidden md:block sticky top-20 w-96 h-screen">
-      <div className="h-min bg-back p-4 rounded-lg">
+    <aside className="hidden md:flex flex-col sticky top-20 w-96 max-h-[70vh]">
+      <div className="h-min bg-back p-4 rounded-lg overflow-y-auto flex-0">
         <NavActions />
       </div>
     </aside>
