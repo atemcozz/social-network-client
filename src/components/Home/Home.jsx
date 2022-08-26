629f4e0d0d34a27ad2b92bcaecafeaa5119f66da
atemcozz <atemcozzartem@gmail.com>
2022-08-26 21:23:37 +0300

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,77 +1,28 @@
 import React from "react";
 import Post from "../Post/Post";
-import ActionSideMenu from "../ActionSideMenu/ActionSideMenu";
+import { API_URL } from "../../api/server";
+import useRequest from "../../hooks/useRequest";
+import axios from "axios";
+import Spinner from "../UI/Spinner/Spinner";
 
 const Home = () => {
-  const posts = [
-    {
-      userNickname: "Вася Пупкин",
-      userAvatarURL:
-        "https://miro.medium.com/max/1024/1*X2hhWhT8LxskswFNZUsMFg.png",
-      geo: "Санкт-Петербург",
-      photoURL:
-        "https://steamuserimages-a.akamaihd.net/ugc/396707750020595720/B41030072317D3387C66738F0E6FB108BBE898E1/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-      description: "Description",
-      likesCount: 2,
-      commentsCount: 12,
-      bookmark: false,
-    },
-    {
-      userNickname: "Вася Пупкин",
-      userAvatarURL:
-        "https://miro.medium.com/max/1024/1*X2hhWhT8LxskswFNZUsMFg.png",
-      geo: "Санкт-Петербург",
-      photoURL:
-        "https://steamuserimages-a.akamaihd.net/ugc/396707750020595720/B41030072317D3387C66738F0E6FB108BBE898E1/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-      description: "Description",
-      likesCount: 534,
-      commentsCount: 232,
-      bookmark: true,
-      liked: true,
-    },
-    {
-      userNickname: "Вася Пупкин",
-      userAvatarURL:
-        "https://miro.medium.com/max/1024/1*X2hhWhT8LxskswFNZUsMFg.png",
-      geo: "Санкт-Петербург",
-      photoURL:
-        "https://steamuserimages-a.akamaihd.net/ugc/396707750020595720/B41030072317D3387C66738F0E6FB108BBE898E1/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-      description: "Description",
-      likesCount: 2,
-      commentsCount: 12,
-      bookmark: false,
-    },
-    {
-      userNickname: "Вася Пупкин",
-      userAvatarURL:
-        "https://miro.medium.com/max/1024/1*X2hhWhT8LxskswFNZUsMFg.png",
-      geo: "Санкт-Петербург",
-      photoURL:
-        "https://steamuserimages-a.akamaihd.net/ugc/396707750020595720/B41030072317D3387C66738F0E6FB108BBE898E1/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-      description: "Description",
-      likesCount: 2,
-      commentsCount: 12,
-      bookmark: false,
-    },
-    {
-      userNickname: "Вася Пупкин",
-      userAvatarURL:
-        "https://miro.medium.com/max/1024/1*X2hhWhT8LxskswFNZUsMFg.png",
-      geo: "Санкт-Петербург",
-      photoURL:
-        "https://steamuserimages-a.akamaihd.net/ugc/396707750020595720/B41030072317D3387C66738F0E6FB108BBE898E1/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-      description: "Description",
-      likesCount: 2,
-      commentsCount: 12,
-      bookmark: false,
-    },
-  ];
+  const [posts, postsLoading, error] = useRequest(() =>
+    axios.get(`${API_URL}/posts`)
+  );
   return (
     <div className="w-full px-4 min-h-screen flex flex-col gap-4">
-      <div className="font-bold text-xl pl-6">Популярное</div>
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
+      {postsLoading ? (
+        <div className="flex items-center justify-center w-full">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <div className="font-bold text-xl pl-6">Популярное</div>
+          {posts.map((post, index) => (
+            <Post key={index} post={post} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,28 +3,58 @@ import Button from "../UI/Button/Button";
 import CheckButton from "../UI/Button/CheckButton";
 import LikeButton from "../UI/Button/LikeButton";
 import { BsChatLeftTextFill, BsBookmark } from "react-icons/bs";
-
+import defaultAvatar from "../../assets/default_avatar.png";
+import imageNotFound from "../../assets/image_notfound.png";
 const Post = ({ post }) => {
   return (
     <div className="flex flex-col rounded-lg shadow-md p-4 bg-back">
       <div className="flex items-center gap-2 mb-2">
         <div className="w-12 h-12">
           <img
-            className="object-cover w-12 h-12 rounded-full"
-            src={post.userAvatarURL}
+            className="object-cover w-12 h-12 rounded-full shadow"
+            src={post.user.avatar_url}
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null;
+              currentTarget.src = defaultAvatar;
+            }}
             alt="avatar"
           />
         </div>
         <div className="flex flex-col">
-          <div className="font-semibold text-lg">{post.userNickname}</div>
-          <div className="font-light text-xs">{post.geo}</div>
+          <div className="font-semibold text-lg">{post.user.nickname}</div>
+          <div className="font-light text-xs">
+            {new Date(post.created_at)
+              .toLocaleDateString("ru-RU", {
+                day: "numeric",
+                month: "numeric",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })
+              .toString()}
+          </div>
         </div>
       </div>
 
-      <div>
-        <img className="w-full rounded-lg" src={post.photoURL} alt="img" />
+      <div className="flex flex-col gap-3">
+        {post.attachments.map((at, index) => {
+          if (at.type === "photo") {
+            return (
+              <img
+                key={index}
+                className="w-full rounded-lg"
+                src={at.url}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null;
+                  currentTarget.src = imageNotFound;
+                }}
+                alt="img"
+              />
+            );
+          }
+        })}
       </div>
-      <div className="p-2 bg-back-lighter rounded-lg shadow -mt-6 mb-4 w-11/12 self-center break-words">
+      <div className="p-2 bg-back-lighter rounded-lg shadow mt-2 mb-4 w-11/12 self-center break-words">
         {post.description}
       </div>
       <div className="flex justify-between items-center ">
