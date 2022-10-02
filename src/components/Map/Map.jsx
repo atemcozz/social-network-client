ea34b7b13124bb3c37943929fdf30af64f4a5a21
atemcozz <atemcozzartem@gmail.com>
2022-10-02 11:24:30 +0300

    zoom fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,6 +6,7 @@ import {
   Popup,
   AttributionControl,
   useMapEvents,
+  useMap,
 } from "react-leaflet";
 import markerIconPng from "../../assets/marker.png";
 import { Icon } from "leaflet";
@@ -19,10 +20,35 @@ import L from "leaflet";
 import useSupercluster from "use-supercluster";
 const Map = ({ center, zoom = 20, locations }) => {
   const { store } = useContext(Context);
-  const mapRef = useRef();
-  const [mapZoom, setMapZoom] = useState(zoom);
+
   const accessToken =
     "2vT72l92FFVGlmkE95lAV5v3Ipiu70TOCcl9eysYedIe7aIyiX6AHxUrHNJQ648o";
+
+  return (
+    <div>
+      <MapContainer center={center} zoom={zoom} attributionControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={
+            store.appTheme === "theme-dark"
+              ? `https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${accessToken}`
+              : `https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=${accessToken}`
+          }
+        />
+        <LocationsCluster locations={locations} startZoom={zoom} />
+        <AttributionControl position="bottomright" prefix={false} />
+      </MapContainer>
+    </div>
+  );
+};
+const LocationsCluster = ({ locations, radius = 150, startZoom }) => {
+  const map = useMap();
+  const [zoom, setZoom] = useState(startZoom);
+  useMapEvents({
+    zoomend(e) {
+      setZoom(e.target._zoom);
+    },
+  });
   const clusterIcon = (count) => {
     const icon = L.divIcon({
       html: `<div class="cluster-marker">
@@ -42,75 +68,56 @@ const Map = ({ center, zoom = 20, locations }) => {
   const { clusters, supercluster } = useSupercluster({
     points: points,
     bounds: [-180, -90, 180, 90],
-    zoom: mapZoom,
-    options: { radius: 150, maxZoom: 20 },
+    zoom: zoom,
+    options: { radius, maxZoom: 20 },
   });
-  const [mapLoaded, setMapLoaded] = useState(false);
-  useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.on("zoomend", (e) => {
-        setMapZoom(e.target._zoom);
-      });
-    }
-  }, [mapLoaded]);
   return (
-    <div>
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        attributionControl={false}
-        ref={mapRef}
-        whenReady={() => setMapLoaded(true)}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={
-            store.appTheme === "theme-dark"
-              ? `https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${accessToken}`
-              : `https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=${accessToken}`
-          }
-        />
-        <AttributionControl position="bottomright" prefix={false} />
-
-        {clusters?.map((cluster) => {
-          const [lng, lat] = cluster.geometry.coordinates;
-          const { cluster: isCluster, point_count: pointCount } =
-            cluster.properties;
-          if (isCluster) {
-            return (
-              <Marker
-                position={{ lat, lng }}
-                key={lat + lng}
-                icon={clusterIcon(pointCount, 32)}
-              >
-                <Popup>
-                  <div className="p-2">Popup</div>
-                </Popup>
-              </Marker>
-            );
-          }
+    <>
+      {" "}
+      {clusters?.map((cluster) => {
+        const [lng, lat] = cluster.geometry.coordinates;
+        const { cluster: isCluster, point_count: pointCount } =
+          cluster.properties;
+        if (isCluster) {
           return (
             <Marker
               position={{ lat, lng }}
               key={lat + lng}
-              icon={
-                new Icon({
-                  iconUrl: markerIconPng,
-                  iconSize: [24, 37],
-                  iconAnchor: [12, 37],
-                  popupAnchor: [0, -37],
-                })
-              }
-            >
-              <Popup>
-                <div className="p-2">Popup</div>
-              </Popup>
-            </Marker>
+              icon={clusterIcon(pointCount, 32)}
+              eventHandlers={{
+                click(e) {
+                  map.flyTo(
+                    { lng, lat },
+                    supercluster.getClusterExpansionZoom(cluster.id),
+                    {
+                      duration: 0.5,
+                    }
+                  );
+                },
+              }}
+            />
           );
-        })}
-      </MapContainer>
-    </div>
+        }
+        return (
+          <Marker
+            position={{ lat, lng }}
+            key={lat + lng}
+            icon={
+              new Icon({
+                iconUrl: markerIconPng,
+                iconSize: [24, 37],
+                iconAnchor: [12, 37],
+                popupAnchor: [0, -37],
+              })
+            }
+          >
+            <Popup>
+              <div className="p-2">Popup</div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </>
   );
 };
-
 export default React.memo(Map);
