821d258668e2ad3a0add9095127e2ff0eb758bb4
atemcozz <atemcozzartem@gmail.com>
2022-10-21 23:03:07 +0300

    ю

--- src/components/FullPost/FullPost.jsx
+++ src/components/FullPost/FullPost.jsx
@@ -19,6 +19,321 @@ const FullPost = () => {
   const [comments, commentsLoading, commentsError, updateComments] = useRequest(
     () => PostService.getComments(id)
   );
+  const fakeComments = [
+    {
+      id: 0,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: null,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 1,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 0,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 2,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 1,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 3,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 2,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 4,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 3,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 5,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 4,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 6,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 5,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 7,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 6,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 8,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 7,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 9,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 8,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 10,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 9,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 11,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 10,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 12,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 11,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 13,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 12,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 14,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 13,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 15,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 14,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 16,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 15,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 17,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 16,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 18,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 17,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+    {
+      id: 19,
+      body: "Тест веток",
+      created_at: "2022-10-21T17:13:54.541Z",
+      post_id: 76,
+      belongsTo: 18,
+      user: {
+        id: 2,
+        name: "Artem",
+        surname: "Kosov",
+        nickname: "atemcozz",
+        avatar_url:
+          "https://res.cloudinary.com/aqemcozz/image/upload/v1666361872/social-network/1666361872390-685705502.jpg",
+      },
+    },
+  ];
+  function arrayToTree(arr, belong = null) {
+    const top = [];
+    arr.forEach((el) => {
+      if (
+        el.belongsTo === belong ||
+        (!el.hasOwnProperty("belongsTo") && belong === null)
+      ) {
+        el.children = arrayToTree(arr, el.id);
+        top.push(el);
+      }
+    });
+    return top;
+  }
   if (commentsLoading || postLoading) {
     return (
       <div className="flex items-center justify-center w-full h-[30vh]">
@@ -27,7 +342,7 @@ const FullPost = () => {
     );
   }
   return (
-    <div className="min-h-screen flex flex-col gap-4">
+    <div className="min-h-screen flex flex-col gap-4 ">
       {/* <div className="flex items-center gap-4">
         <button
           className="flex items-center bg-back hover:bg-back-darker p-2 rounded-full shadow text-primary"
@@ -66,15 +381,17 @@ const FullPost = () => {
           </div>
         </>
       )}
-      {comments?.length > 0 ? (
-        comments.map((comment, index) => (
-          <Comment key={index} comment={comment} onChange={updateComments} />
-        ))
-      ) : (
-        <div className="rounded-lg p-2 bg-back-lighter">
-          Никто ещё не оставил комментариев, станьте первым!
-        </div>
-      )}
+      <div className="flex flex-col gap-2 overflow-auto">
+        {fakeComments?.length > 0 ? (
+          arrayToTree(fakeComments).map((comment, index) => (
+            <Comment key={index} comment={comment} onChange={updateComments} />
+          ))
+        ) : (
+          <div className="rounded-lg p-2 bg-back-lighter">
+            Никто ещё не оставил комментариев, станьте первым!
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Post/Comment.jsx
+++ src/components/Post/Comment.jsx
@@ -8,53 +8,105 @@ import { MdDeleteForever } from "react-icons/md";
 import { Context } from "../../index";
 import LinkText from "../UI/LinkText/LinkText";
 import { Link } from "react-router-dom";
-const Comment = ({ comment, onChange }) => {
+import { BsArrowUpSquare } from "react-icons/bs";
+import { useState } from "react";
+import Button from "../UI/Button/Button";
+const Comment = ({ comment, onChange, depth = 0 }) => {
   const { store } = useContext(Context);
+  const [branchVisible, setBranchVisible] = useState(true);
+  const [branchHover, setBranchHover] = useState(false);
   async function deleteComment() {
     await PostService.deleteComment(comment.id)
       .then(() => onChange())
       .catch((e) => console.error(e));
   }
+
   const navigate = useNavigate();
   return (
-    <div className="flex p-2 bg-back shadow rounded-lg items-start justify-between">
-      <div className="flex gap-2">
-        <Link to={`/user/${comment.user.id}`} target={"_blank"}>
-          <Avatar src={comment.user.avatar_url} size="normal" />
-        </Link>
-        <div className="flex flex-col">
-          <div className="flex flex-wrap items-center gap-2">
-            <Link
-              target={"_blank"}
-              to={`/user/${comment.user.id}`}
-              className="font-semibold text-base cursor-pointer hover:text-primary"
-            >
-              {comment.user.nickname}
-            </Link>
+    <div className={`flex flex-col gap-2 min-w-max`}>
+      <div className="flex p-2 bg-back shadow rounded-lg items-start justify-between">
+        <div className="flex gap-2">
+          <Link to={`/user/${comment.user.id}`} target={"_blank"}>
+            <Avatar src={comment.user.avatar_url} size="normal" />
+          </Link>
+          <div className="flex flex-col">
+            <div className="flex flex-wrap items-center gap-2">
+              <Link
+                target={"_blank"}
+                to={`/user/${comment.user.id}`}
+                className="font-semibold text-base cursor-pointer hover:text-primary"
+              >
+                {comment.user.nickname}
+              </Link>
 
-            {/* <div className="font-semibold text-base cursor-pointer hover:text-primary">
+              {/* <div className="font-semibold text-base cursor-pointer hover:text-primary">
               {comment.user.nickname}
             </div> */}
-            <div className="font-light text-xs leading-3">
-              {getDateFromSQL(comment.created_at)}
+              <div className="font-light text-xs leading-3">
+                {getDateFromSQL(comment.created_at)}
+              </div>
+            </div>
+            <div className="break-words max-w-md">
+              <LinkText>{comment.body}</LinkText>
             </div>
           </div>
-          <div className="break-words max-w-md">
-            <LinkText>{comment.body}</LinkText>
-          </div>
         </div>
-      </div>
 
-      <DotsDropdown>
-        {store.user?.id === comment.user.id && (
-          <DotsDropdown.Item
-            icon={<MdDeleteForever size={"24px"} />}
-            onClick={deleteComment}
+        <DotsDropdown>
+          {store.user?.id === comment.user.id && (
+            <DotsDropdown.Item
+              icon={<MdDeleteForever size={"24px"} />}
+              onClick={deleteComment}
+            >
+              Удалить
+            </DotsDropdown.Item>
+          )}
+        </DotsDropdown>
+      </div>
+      {!branchVisible && (
+        <Button variant="outlined" onClick={() => setBranchVisible(true)}>
+          Раскрыть ветку
+        </Button>
+      )}
+      {comment.children?.length > 0 && (
+        <div
+          className={`${!branchVisible && "hidden"}
+           ${
+             branchHover ? "border-primary" : "border-secondary-darker"
+           } ml-2 pl-4 border-l-2  flex flex-col gap-2 relative`}
+        >
+          <div
+            onMouseOver={(e) => {
+              e.stopPropagation();
+
+              setBranchHover(true);
+            }}
+            onMouseOut={(e) => {
+              e.stopPropagation();
+              setBranchHover(false);
+            }}
+            className="cursor-pointer absolute inset-y-0 -left-2 w-4"
+            onClick={() => setBranchVisible(false)}
           >
-            Удалить
-          </DotsDropdown.Item>
-        )}
-      </DotsDropdown>
+            <div
+              className={`${
+                branchHover ? "text-primary" : "text-secondary-darker"
+              } absolute top-6  bg-back-darker`}
+            >
+              <BsArrowUpSquare />
+            </div>
+          </div>
+
+          {comment.children.map((child, index) => (
+            <Comment
+              key={index}
+              comment={child}
+              onChange={onChange}
+              depth={depth + 1}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
