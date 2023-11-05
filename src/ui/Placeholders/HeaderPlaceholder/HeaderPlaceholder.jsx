5778f49d9c884a72fdc5da3cb4692ac01b9d9ff6
atemcozz <atemcozzartem@gmail.com>
2023-11-05 20:49:59 +0300

    layout fixes

--- src/ui/Placeholders/HeaderPlaceholder/HeaderPlaceholder.jsx
+++ src/ui/Placeholders/HeaderPlaceholder/HeaderPlaceholder.jsx
@@ -10,7 +10,7 @@ const HeaderPlaceholder = ({page}) => {
     <header className="bg-back shadow-lg w-full fixed h-16 top-0 left-0 z-20">
 
       <div className="flex justify-between items-center mx-auto h-full max-w-screen-xl py-2 ">
-        <div className="flex items-center gap-3 w-64 mx-4">
+        <div className="flex items-center gap-3 flex-1 mx-4">
           <div className="flex items-center ">
             <div className=" text-2xl font-bold text-text-base">Falco</div>
             <FalcoIcon width={32} height={32}/>
@@ -21,7 +21,7 @@ const HeaderPlaceholder = ({page}) => {
           <ButtonPlaceholder/>
           <ButtonPlaceholder/>
         </div>
-        <div className={"hidden md:flex gap-2 items-center w-64 justify-center mx-4"}>
+        <div className={"hidden md:flex gap-2 items-center flex-1 justify-center mx-4"}>
           <ButtonPlaceholder/>
         </div>
 
